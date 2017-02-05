f2f9338cf4f1feb09580411a95bfa17702122cd1
Mateusz Molka <b.0910.molmat@gmail.com>
2017-02-05 11:46:04 +0100

    2-Search Elements

---
+++ js/2-search-element.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var parFirst=document.getElementById("parFirst");
+console.log(parFirst);
+
+var link= document.getElementsByClassName("link");
+console.log(link);
+
+var linkPoTagu= document.getElementsByTagName("a");
+console.log(linkPoTagu);
+
+var linkiPoSelektorze= document.querySelectorAll('.superlink');
+console.log(linkiPoSelektorze);
+
+var divPoId= document.querySelector('#parSecond');
+console.log(divPoId);
